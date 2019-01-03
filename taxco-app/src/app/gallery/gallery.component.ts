60dcf7377cf67492a20ac45ff7b7e449795762df
Diego Coin Rodriguez <dcoin@ofimadrid.priv>
2019-01-03 17:26:00 +0100

    Taxco - Gallery changes for pagination

--- taxco-app/src/app/gallery/gallery.component.ts
+++ taxco-app/src/app/gallery/gallery.component.ts
@@ -12,6 +12,8 @@ export class GalleryComponent implements OnInit {
 
   private images: Image[];
   private maxPerPage: number;
+  private numPage: number;
+  private actualPage: number;
   temp = Array;
   math = Math;
 
@@ -39,8 +41,53 @@ export class GalleryComponent implements OnInit {
       new Image('/assets/resources/images/presentation.jpg', 'Prueba19', 'Pabellon'),
       new Image('/assets/resources/images/presentation.jpg', 'Prueba20', 'Hueso'),
       new Image('/assets/resources/images/logo.jpg', 'Prueba21', 'Entero'),
-      new Image('/assets/resources/images/presentation.jpg', 'Prueba22', 'Pecho')];
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba22', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba1', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba2', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba3', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba4', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba5', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba6', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba7', 'Pecho'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba8', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba9', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba10', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba11', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba12', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba13', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba14', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba15', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba16', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba17', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba18', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba19', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba20', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba21', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba1', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba2', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba3', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba4', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba5', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba6', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba7', 'Pecho'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba8', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba9', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba10', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba11', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba12', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba13', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba14', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba15', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba16', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba17', 'Entero'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba18', 'Pecho'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba19', 'Pabellon'),
+      new Image('/assets/resources/images/presentation.jpg', 'Prueba20', 'Hueso'),
+      new Image('/assets/resources/images/logo.jpg', 'Prueba21', 'Entero')
+    ];
     this.maxPerPage= (this.images.length/3) > 5 ? 5 : this.images.length/3;
+    this.numPage= this.images.length/15;
+    this.actualPage= 1;
   }
 
   ngOnInit() {
