ec69623f55415b9976fb2ed6a71b9d050b6d2913
Diego Coin <diegocoin1992@gmail.com>
2019-03-17 18:36:27 +0100

    Taxco - firebase integration first steps

--- taxco-app/src/environments/environment.prod.ts
+++ taxco-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCn8R5ojJ4IXqwpBseAR2tbiQoF4D3pev4",
+      authDomain: "taxco-app.firebaseapp.com",
+      databaseURL: "https://taxco-app.firebaseio.com",
+      projectId: "taxco-app",
+      storageBucket: "taxco-app.appspot.com",
+      messagingSenderId: "3588211874"
+  }
 };
