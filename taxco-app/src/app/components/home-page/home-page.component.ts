291b45c87de08ac32a710141a7aedb8a1b3283e5
Diego Coin Rodriguez <dcoin@ofimadrid.priv>
2019-06-05 12:32:27 +0200

    Taxco - Map with marker and more general style changes

--- taxco-app/src/app/components/home-page/home-page.component.ts
+++ taxco-app/src/app/components/home-page/home-page.component.ts
@@ -18,7 +18,6 @@ declare let L;
 export class HomePageComponent implements OnInit {
 
   videoUrl : string = 'https://www.youtube.com/watch?v=DBLFwHosi2c';
-  map : any;
   latitude : number = 40.3949902; 
   longitude : number = -3.7335585;
 
@@ -41,7 +40,20 @@ export class HomePageComponent implements OnInit {
       source: source
     });*/
 
-    this.map = new Map({
+    //var mymap = L.map('mapid').setView([this.longitude, this.latitude], 16);
+    const mymap = L.map('mapid').setView([this.latitude, this.longitude], 16);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZG9udGluc2VydGNvaW4iLCJhIjoiY2p3ODNpbDZlMDJkdjN5bXgwb2F3OWpsNiJ9.lyJhHXeNT2P4lSArgrjO4w'
+    }).addTo(mymap);
+
+    let marker = L.marker([this.latitude, this.longitude]).addTo(mymap);
+    marker.bindPopup("Taxidermia Coin SL").openPopup();
+    
+    /*new Map({
       target: 'map',
       layers: [
         new TileLayer({
@@ -53,7 +65,7 @@ export class HomePageComponent implements OnInit {
         center: fromLonLat([this.longitude, this.latitude]),
         zoom: 16
       })
-    });
+    });*/
     //const mark = L.marker([this.longitude, this.latitude],this.icon).addTo(this.map);
 
     //var marker = L.marker([37.7858, -122.401], { title: "My marker" }).addTo(map);
