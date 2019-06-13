1d07bfa3b636359ff45a6ae6cc6e73a91bb223f6
Diego Coin Rodriguez <dcoin@ofimadrid.priv>
2019-06-13 12:32:56 +0200

    Taxco - Carousel with photos and text and small fixes

--- taxco-app/src/app/components/home-page/carousel/carousel.component.ts
+++ taxco-app/src/app/components/home-page/carousel/carousel.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images = ['https://firebasestorage.googleapis.com/v0/b/taxco-app.appspot.com/o/carrusel-image-1.png?alt=media&token=e181b2a5-501f-44c0-a6a7-81cc0e77fbd3',
+  'https://firebasestorage.googleapis.com/v0/b/taxco-app.appspot.com/o/carrusel-image-2.png?alt=media&token=5d540de6-8177-40ca-9944-a07698a2c9a3',
+  'https://firebasestorage.googleapis.com/v0/b/taxco-app.appspot.com/o/carrusel-image-3.png?alt=media&token=e923e92d-74d1-45b8-bd2a-6e5be2cf964f']
 
   constructor() { }
 
