3f07c8072e625efd0bb72d3ef97286a886f61420
Roman Zukov <roman.zukov@mitulagroup.com>
2019-06-20 12:46:33 +0200

    Taxco - Jenkins config refactor v2

--- taxco-app/src/karma.conf.js
+++ taxco-app/src/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chrome', 'PhantomJS'],
+    singleRun: true
   });
 };
